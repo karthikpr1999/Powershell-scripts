2f236aef8abda1b9169d5f3fa5c5e9027bf2b24d
karthikpr1999 <71974799+karthikpr1999@users.noreply.github.com>
2026-05-20 18:54:29 +0530

    Update Hourly_LM_email.ps1

--- Hourly_LM_email.ps1
+++ Hourly_LM_email.ps1
@@ -2,9 +2,9 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 # ===== CONFIGURATION =====
-$LogicMonitorURL = "https://sap.logicmonitor.com/santaba/uiv4/resources/treeNodes?resourcePath=resourceGroups-1%2A%2CresourceGroups-17%2CresourceGroups-25"
+$LogicMonitorURL = ""
 $IntervalSeconds = 10   # 1 hour
-$LogFile = "C:\Users\C5340448\OneDrive - SAP SE\Powershell_scripts\logic_monitor_logs\HourLogicMonitor.log"
+$LogFile = ""
 
 # ===== SYSTEM TRAY ICON =====
 $notify = New-Object System.Windows.Forms.NotifyIcon
